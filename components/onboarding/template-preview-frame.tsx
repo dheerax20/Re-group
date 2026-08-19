b3f85ea33d41d1369e7cd8af9e32e04d5ebe48fd
dheerax20 <sahdheeraj19@gmail.com>
2026-08-19 13:50:32 +0530

    Re: Website Builder

--- components/onboarding/template-preview-frame.tsx
+++
@@ -1,9 +0,0 @@
-export function TemplatePreviewFrame({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="h-[420px] overflow-hidden rounded-t-lg border-b border-border bg-surface">
-      <div className="pointer-events-none w-[285.7%] origin-top-left scale-[0.35]">
-        {children}
-      </div>
-    </div>
-  );
-}
